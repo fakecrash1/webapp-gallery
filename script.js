dd75b8486ac546eeecedc3d4df21575c1cdeb382
fakecrash1 <fakecrash@gmail.com>
2023-05-10 23:18:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,47 @@ for(i = 0; i < thumbnails.length; i++) {
     thumbnails[i].className = "thumbnail-" + i;
 }
 
-let data = {
-    photo: 'images/AdobeStock_01.jpeg',
-    title: 'My title',
-    description: 'What happened here, why is this a very nice image'
-};
+let imagesData = [
+    {
+        photo: "images/AdobeStock_01.jpeg",
+        title: "My title 1",
+        description: "What happened here, why is this a very nice image 1"
+    },
+    {
+        photo: "images/AdobeStock_02.jpeg",
+        title: "My title 2",
+        description: "What happened here, why is this a very nice image 2"
+    },
+    {
+        photo: "images/AdobeStock_03.jpeg",
+        title: "My title 3",
+        description: "What happened here, why is this a very nice image 3"
+    },
+    {
+        photo: "images/AdobeStock_04.jpeg",
+        title: "My title 4",
+        description: "What happened here, why is this a very nice image 4"
+    },
+    {
+        photo: "images/AdobeStock_05.jpeg",
+        title: "My title 5",
+        description: "What happened here, why is this a very nice image 5"
+    },
+    {
+        photo: "images/AdobeStock_06.jpeg",
+        title: "My title 6",
+        description: "What happened here, why is this a very nice image 6"
+    },
+    {
+        photo: "images/AdobeStock_07.jpeg",
+        title: "My title 7",
+        description: "What happened here, why is this a very nice image 7"
+    },
+    {
+        photo: "images/AdobeStock_08.jpeg",
+        title: "My title 8",
+        description: "What happened here, why is this a very nice image 8"
+    }
+];
 
 $('#photo').attr('src', data.photo);

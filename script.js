4fc29b9fb86f165e7abba8b02c93aa1478c1a29b
fakecrash1 <fakecrash@gmail.com>
2023-05-10 13:17:58 +0200

    following description of webapp
    Kapott kód
    """
    let data = {
      photo: 'images/photo1.jpg',
      title: 'My title',
      description: 'What happened here, why is this a very nice image'
    };
    
    $('#photo').attr('src', data.photo);
    """

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ for(i = 0; i < thumbnails.length; i++) {
     thumbnails[i].className = "thumbnail-" + i;
 }
 
+let data = {
+    photo: 'images/AdobeStock_01.jpeg',
+    title: 'My title',
+    description: 'What happened here, why is this a very nice image'
+};
+
+$('#photo').attr('src', data.photo);

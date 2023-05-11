7012b0acf2bd86d7612036c6712ec3c91ad46e72
fakecrash1 <fakecrash@gmail.com>
2023-05-11 05:34:58 +0200

    fixing up the problems
    This wanted to be a function, but because scripts was not injected well to html, i did not see what it caused, now that it is finally good, i can fixing up things and get results after problems.

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-var thumbnails = document.getElementsByClassName("thumbnail")
-
-for(i = 0; i < thumbnails.length; i++) {
-    thumbnails[i].className = "thumbnail-" + i;
-}
 
 let imagesData = [
     {

e9774d842fd5b717918967c4ab81e85770201103
fakecrash1 <fakecrash@gmail.com>
2023-05-11 05:15:55 +0200

    experimenting
    I dont know why it now changes the photo

--- script.js
+++ script.js
@@ -4,71 +4,73 @@ for(i = 0; i < thumbnails.length; i++) {
     thumbnails[i].className = "thumbnail-" + i;
 }
 
-let imagesData = [
-    {
-        photo: "images/AdobeStock_01.jpeg",
-        title: "Almost",
-        description: "After the picture i fellt into the water"
-    },
-    {
-        photo: "images/AdobeStock_02.jpeg",
-        title: "DJ",
-        description: "Coding is like party all day isn't it?"
-    },
-    {
-        photo: "images/AdobeStock_03.jpeg",
-        title: "Seeds",
-        description: "Every line written is like a seed planted"
-    },
-    {
-        photo: "images/AdobeStock_04.jpeg",
-        title: "Some rest",
-        description: "Sometime everyone needs a vacation"
-    },
-    {
-        photo: "images/AdobeStock_05.jpeg",
-        title: "Long road",
-        description: "Coding seems to be a long journey.."
-    },
-    {
-        photo: "images/AdobeStock_06.jpeg",
-        title: "Nature",
-        description: "There is always a way"
-    },
-    {
-        photo: "images/AdobeStock_07.jpeg",
-        title: "Yoga",
-        description: "Recharge yourself is important"
-    },
-    {
-        photo: "images/AdobeStock_08.jpeg",
-        title: "Keep research",
-        description: "Keep research to be as up to date as possible"
+$(document).ready(function () {
+    let imagesData = [
+        {
+            photo: "images/AdobeStock_01.jpeg",
+            title: "Almost",
+            description: "After the picture i fellt into the water"
+        },
+        {
+            photo: "images/AdobeStock_02.jpeg",
+            title: "DJ",
+            description: "Coding is like party all day isn't it?"
+        },
+        {
+            photo: "images/AdobeStock_03.jpeg",
+            title: "Seeds",
+            description: "Every line written is like a seed planted"
+        },
+        {
+            photo: "images/AdobeStock_04.jpeg",
+            title: "Some rest",
+            description: "Sometime everyone needs a vacation"
+        },
+        {
+            photo: "images/AdobeStock_05.jpeg",
+            title: "Long road",
+            description: "Coding seems to be a long journey.."
+        },
+        {
+            photo: "images/AdobeStock_06.jpeg",
+            title: "Nature",
+            description: "There is always a way"
+        },
+        {
+            photo: "images/AdobeStock_07.jpeg",
+            title: "Yoga",
+            description: "Recharge yourself is important"
+        },
+        {
+            photo: "images/AdobeStock_08.jpeg",
+            title: "Keep research",
+            description: "Keep research to be as up to date as possible"
+        }
+    ];
+
+    let currentPhoto = 0;
+
+    function loadPhoto(photoNumber) {
+        $('#photo').attr('src', imagesData[photoNumber].photo);
+        $('#photo-title').text(imagesData[photoNumber].title);
+        $('#photo-description').text(imagesData[photoNumber].description);
     }
-];
 
-let currentPhoto = 0;
-
-function loadPhoto(photoNumber) {
-    $('#photo').attr('src', imagesData[photoNumber].photo);
-    $('#photo-title').text(imagesData[photoNumber].title);
-    $('#photo-description').text(imagesData[photoNumber].description);
-}
-
-loadPhoto(currentPhoto);
-
-$('#right-arrow').click(() => {
-    currentPhoto++;
-    if (currentPhoto >= imagesData.length) {
-    currentPhoto = 0;
-    }
     loadPhoto(currentPhoto);
-});
 
-$('#left-arrow').click(() => {
-    currentPhoto--;
-    if (currentPhoto < 0) {
-    currentPhoto = imagesData.length - 1;
-    }
-    loadPhoto(currentPhoto);
+    $('#right-arrow').click(() => {
+        currentPhoto++;
+        if (currentPhoto >= imagesData.length) {
+        currentPhoto = 0;
+        }
+        loadPhoto(currentPhoto);
+    });
+
+    $('#left-arrow').click(() => {
+        currentPhoto--;
+        if (currentPhoto < 0) {
+        currentPhoto = imagesData.length - 1;
+        }
+        loadPhoto(currentPhoto);
+    });
 });
